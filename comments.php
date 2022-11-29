5fddb4895f47d1e3e75bb45780ded6e202401d2e
HBatistuzzo <hbatistuzzo@gmail.com>
2022-11-29 10:30:00 -0300

    files

---
+++ comments.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+// This function below displays text. This comment needs to be inside the php to work. And this only does one line
+
+    echo "Hello Student how are you";
+
+/*
+and this
+is a multiline
+comment. Just like in... python? I think?
+*/
+
+?>
+    
+</body>
+</html>

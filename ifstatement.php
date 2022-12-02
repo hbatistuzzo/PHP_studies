d54b35af19de9cf7f7f4da29747e70b3ce51f969
HBatistuzzo <hbatistuzzo@gmail.com>
2022-12-01 21:31:52 -0300

    Create ifstatement.php

---
+++ ifstatement.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php>
+
+/* Below is the basic structure. If what's inside the parentesis yields True, then execute everything inside the curly brackets.
+if(){
+
+}
+*/
+
+if(3 < 10){
+echo "yes, 3 is less than 10"
+}
+*/
+
+?>
+    
+</body>
+</html>
